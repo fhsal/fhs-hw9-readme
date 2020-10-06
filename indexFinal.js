e9f6b39bcf165418af11e9b2a7079b2fd466f318
frank salvo <fhsalvo@yahoo.com>
2020-10-05 23:03:47 -0400

    renamed index15b to indexFinal and changed name of created file to sampleReadMe.  Seems fully functional

---
+++ indexFinal.js
@@ -0,0 +1,147 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+var badge;
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the Title of your project?"
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "please provide a description of your application",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "enter usage information for your app"
+    }, 
+    {
+      type: "input",
+      name: "contribution",
+      message: "enter information regarding contributors to your app"
+    }, 
+    {
+      type: "list",
+      name: "licensing",
+      message: "what type of licencing for this package?",
+      choices: ['GNU', 'APACHE', 'BSD', 'MIT']
+    },   
+    {
+      type: "input",
+      name: "installation",
+      message: "enter the installation instructions for your app"
+    }, 
+    {
+      type: "input",
+      name: "testing",
+      message: "how is testing to be performed for the application?",
+    },
+    {
+      type: "input",
+      name: "gitHub",
+      message: "Enter your GitHub profile url"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email"
+    }
+  ])
+}
+
+
+
+function generateContent(answers) {
+  return `
+
+Project Title :  ${answers.title}        ${badge}             
+=================================
+
+Table of Contents: 
+
+- [Appliction Description](#description)
+
+- [Installation Instructions](#installation-instructions)
+
+- [Usage](#usage)
+
+- [Licensing](#licensing-type)
+
+- [Contributions](#contributions)
+
+- [Testing](#testing)
+
+- [GitHub-profile](#github-profile)
+
+- [Questions](#questions)
+
+
+## Description
+
+${answers.description} 
+
+## Installation Instructions
+
+${answers.installation}
+
+## Usage
+
+${answers.usage} 
+
+## Licensing Type
+
+${answers.licensing}
+
+## Contributions
+
+${answers.contribution}
+
+## Testing
+
+${answers.testing}
+
+## GitHub profile
+
+[link](${answers.gitHub}
+
+## Questions
+I can be reached at ${answers.email}, all inquiries will be handled on a best-effort basis.  We make every effort
+to respond within 48 hours. 
+`;
+}
+
+promptUser()
+  .then(function(answers) {
+    if (answers.licensing == 'GNU'){
+        badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)';
+        ;}
+    else if(answers.licensing =='APACHE'){
+        badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        ;}
+    else if(answers.licensing =='BSD'){
+        badge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+        ;}
+    else if(answers.licensing =='MIT'){
+        badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        ;};
+
+    const text = generateContent(answers);
+    return writeFileAsync("sampleReadMe.md", text);
+  })
+  .then(function() {
+    console.log("Successfully wrote to sampleReadMe.md");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
---
+++ sampleReadMe.md
@@ -0,0 +1,55 @@
+
+
+Project Title :  Bedrock Quarry Upgrade, v2.0        [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)             
+=================================
+
+Table of Contents: 
+
+- [Appliction Description](#description)
+
+- [Installation Instructions](#installation-instructions)
+
+- [Usage](#usage)
+
+- [Licensing](#licensing-type)
+
+- [Contributions](#contributions)
+
+- [Testing](#testing)
+
+- [GitHub-profile](#github-profile)
+
+- [Questions](#questions)
+
+
+## Description
+
+modernize the brotosaurus rock lifting apparatas 
+
+## Installation Instructions
+
+only install at night 
+
+## Usage
+
+use carefully and only by trained dinosaur riders 
+
+## Licensing Type
+
+APACHE
+
+## Contributions
+
+Wilma, Betty, Barney, BamBam and Pebbles
+
+## Testing
+
+lift small rocks at first and then increase to 20 tons, 3 tons at a time
+
+## GitHub profile
+
+[link](http://bedrock.com
+
+## Questions
+I can be reached at fred@quarry.rocks, all inquiries will be handled on a best-effort basis.  We make every effort
+to respond within 48 hours. 
